5a07ff5a3fb340dd4ede1ded897a4f765f5640fd
Lskyz <163709104+Lskyz@users.noreply.github.com>
2025-07-29 16:38:34 +0900

    Add files via upload

---
+++ MyBrowserApp/mybrowesrapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mybrowesrapp
+//
+//  Created by 이근용 on 2025/07/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyBrowserApp/mybrowesrapp/mybrowesrappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mybrowesrappApp.swift
+//  mybrowesrapp
+//
+//  Created by 이근용 on 2025/07/29.
+//
+
+import SwiftUI
+
+@main
+struct mybrowesrappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
