4e29488fcea96592f30902eba76e8796be0073f4
Lskyz <163709104+Lskyz@users.noreply.github.com>
2025-07-29 23:27:20 +0900

    Delete MyBrowserApp directory

--- MyBrowserApp/WebViewStateModel.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-class WebViewStateModel: ObservableObject {
-    @Published var canGoBack = false
-    @Published var canGoForward = false
-    @Published var currentURL: URL?
-    @Published var bookmarks: [URL] = []
-}
